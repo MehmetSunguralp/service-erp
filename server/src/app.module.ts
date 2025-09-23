649837170c6b87b0165de44bf479a46322848d95
Mehmet Sunguralp <mehmetsunguralp54@gmail.com>
2025-09-23 23:00:26 +0300

    Added me query

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,9 @@ import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [
@@ -15,10 +17,13 @@ import { JwtModule } from '@nestjs/jwt';
     }),
     UserModule,
     AuthModule,
+    PrismaModule,
     JwtModule.register({
       secret: 'supersecretkey', //TODO: Update this in production
       signOptions: { expiresIn: '1h' },
     }),
   ],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,18 +1,29 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { User } from './user.model';
 import { UserService } from './user.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from '../auth/gql-auth.guard';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
 
   @Query(() => [User])
   @UseGuards(GqlAuthGuard)
   async users() {
     return this.userService.findAll();
+  }
+
+  @UseGuards(GqlAuthGuard)
+  @Query(() => User)
+  async me(@Context() context) {
+    const userId = context.req.user.id; // JwtStrategy'deki "id"
+    return this.prisma.user.findUnique({ where: { id: userId } });
   }
 
   @Mutation(() => User)
